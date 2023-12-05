c64c077e216ea903430d5a4d1b7d82d9969c7a0b
CodeJuzi <d1741530592@163.com>
2023-12-05 22:57:20 +0800

    CodeJuzi, init project

---
+++ src/main/java/io/github/dingxinliang88/MelodyEpochApplication.java
@@ -0,0 +1,16 @@
+package io.github.dingxinliang88;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * APP
+ *
+ * @author <a href="https://github.com/dingxinliang88">codejuzi</a>
+ */
+@SpringBootApplication
+public class MelodyEpochApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MelodyEpochApplication.class, args);
+    }
+}
